30b9ebb7e59a7f517889bc3788cf11dd838334b3
Touza <touza.soukaina@gmail>
2019-01-08 01:11:02 +0100

    dynamic books

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,40 +30,6 @@ class HomeController extends Controller
     }
 
 
-    public function Add(Request $request){
-
-        $this->validate($request, [
-            'titre'=>'required',
-            'genre'=>'required',
-            'auteur'=>'required',
-            'edition'=>'required',
-            'description'=>'required'
-            ]);
-
-        $book = new Book;
-        $book->titre= ucfirst($request->input('titre'));
-        $book->genre=  ucfirst($request->input('genre'));
-        $book->auteur= ucfirst($request->input('auteur'));
-        $book->edition=  ucfirst($request->input('edition'));
-        $book->description=  ucfirst($request->input('description'));
-        
-       /******Verify input admin to avoid insering duplicata****/
-       
-        $verify=Book::where(ucfirst('titre'), $book->titre)->get();
-        if($verify->count() > 0) {
-
-            return  redirect('/AddBook')->with('error','Car already existe!'); 
-        }
-
-        else {
-
-            $book->save();
-            return redirect('/')->with('info','created');
-       } 
-        
-    }
-
-
 
 
 }
